316d7962c7804ecc44dc7dd1e82b25c38e9c070a
Mampacuk <alexanderisraelyan@gmail.com>
2021-10-26 23:41:08 +0400

    finished

--- HW03/ex05/main.cpp
+++ HW03/ex05/main.cpp
@@ -6,7 +6,7 @@
 /*   By: aisraely <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/26 21:40:34 by aisraely          #+#    #+#             */
-/*   Updated: 2021/10/26 22:52:19 by aisraely         ###   ########.fr       */
+/*   Updated: 2021/10/26 23:26:37 by aisraely         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,20 @@ int	main(void)
 {
 	StringArrayPositionalList	seq;
 
-	seq.insertFront(std::string("ho"));
-	seq.insertFront(std::string("hai"));
-	seq.insertFront(std::string("hasdasdi"));
-	seq.insertBack(std::string("no"));
-	seq.insertBack(std::string("no"));
-	seq.insertBack(std::string("no"));
-	seq.insertFront(std::string("yes"));
-	seq.insertFront(std::string("yes"));
-	seq.eraseFront();
-	seq.eraseFront();
-	seq.eraseBack();
+	seq.insertBack(std::string("f1Ecs"));
+	seq.insertBack(std::string("NU7Py"));
+	seq.insertBack(std::string("Gq9"));
+	seq.insertBack(std::string("Peh"));
+	seq.insertBack(std::string("Mz8Yh"));
+	seq.insertBack(std::string("MGtBq"));
+	seq.insertBack(std::string("l20Yb"));
 	seq.print();
+	
+	StringArrayPositionalList::SortedIterator	it = seq.beginSorted();
+	
+	for (int i = 0; i < 7; i++)
+	{
+		std::cout << i << ": " << *it << std::endl;
+		++it;
+	}
 }
